787c2e457a47cf06a7a07e313a9ae7c2035e8251
Simon <hutch120@gmail.com>
2021-07-19 11:44:37 +1000

    Folder structure

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint', // https://github.com/eslint/eslint/issues/10137
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import React from 'react'
+
+function App () {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          FundOSS
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
